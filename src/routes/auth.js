6b855f27bd89ecc5144e6e557a95c65b730615c1
Josué Anderson <47667368+joshuacba08@users.noreply.github.com>
2022-07-29 12:41:14 -0300

    se agrega avatar

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -5,7 +5,7 @@ const router = Router();
 const user = {
     email:'juan.cruz@gmail.com',
     password:'Abc123',
-    avatar:'',
+    avatar:'https://images.pexels.com/photos/2379004/pexels-photo-2379004.jpeg',
     fname:'juan',
     lname:'cruz',
     id:'2'
